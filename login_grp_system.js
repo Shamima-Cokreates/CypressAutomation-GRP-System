c7d92de0268d5c44a083f3d5279db9895763903e
Shamima Nasrin Nila <48233037+Shamima-Cokreates@users.noreply.github.com>
2020-06-10 15:25:15 +0600

    Add files via upload

---
+++ login_grp_system.js
@@ -0,0 +1,19 @@
+context('GRP Login', function() {
+    it('TC-1: View GRP System', function() 
+    {
+    cy.visit('http://sqa.grp.gov.bd/global/web/login')
+    cy.title().should('eq','জিআরপি | ড্যাশবোর্ড') // View GRP dashboard 
+    })
+  
+    it('TC-2: Verify GRP System login scenario', function() 
+      {
+      cy.get('.mb-3 > .form-control').type('nila0001').click() // enter valid username
+      cy.wait(1000)
+      cy.get('.mb-4 > .form-control').type('123456789').click()//enter valid password
+      cy.wait(1000)
+      cy.get('.col-6 > .btn').click()
+    })
+
+
+ 
+})
